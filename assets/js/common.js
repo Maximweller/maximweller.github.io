931cc956f7dee02042a66b607fa393f65572bb9e
Maximweller <Maximweller@users.noreply.github.com>
2018-06-23 19:24:19 +0400

    xgames alpha

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,19 @@
 $(window).on('load', function () {
     $preloader = $('.loading-wrapper'),
-        $loader = $preloader.find('.loader');
+    $loader = $preloader.find('.loader');
     $loader.fadeOut();
     $preloader.delay(350).fadeOut('slow');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
