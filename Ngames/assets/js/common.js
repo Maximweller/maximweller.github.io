df90a46b4b45bd6462f439cf270dc0005da50bdb
Maximweller <Maximweller@users.noreply.github.com>
2018-08-19 22:03:38 +0400

    sum

--- Ngames/assets/js/common.js
+++ Ngames/assets/js/common.js
@@ -1,6 +1,17 @@
 $(window).on('load', function () {
     $preloader = $('.loading-wrapper'),
-        $loader = $preloader.find('.loader');
+    $loader = $preloader.find('.loader');
     $loader.fadeOut();
     $preloader.delay(350).fadeOut('slow');
 });
+
+
+
+
+
+
+
+
+
+
+
